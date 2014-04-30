92b627bb005986ea2c9ee715428ccee8d2c3e473
shivukumar <shivukumarpatil1@gmail.com>
2014-04-30 23:05:46 +0530

    Correcting pir sensor

--- openhab_pir.py
+++ openhab_pir.py
@@ -1,15 +1,15 @@
 import RPi.GPIO as GPIO
 
 def print_detect(channel):
-    f = open("pir_count.txt", "r")
-    count = f.read()
-    count = int(count)
-    count  += 1
-    print "PIR trigger detected count = " + str(count)
-f.close()
-f = open("pir_count.txt", "w")
-f.write(str(count))
-f.close()
+	f = open("pir_count.txt", "r")
+	count = f.read()
+	count = int(count)
+	count  += 1
+	print "PIR trigger detected count = " + str(count)
+	f.close()
+	f = open("pir_count.txt", "w")
+	f.write(str(count))
+	f.close()
 
 
 def main():
@@ -19,9 +19,9 @@ def main():
 
 
 if __name__ == "__main__":
-    f = open("pir_count.txt", "w")
-    f.write("0")
-    f.close()
+	f = open("pir_count.txt", "w")
+	f.write("0")
+	f.close()
 	main()
 	while(True):
-        pass
+		pass
